c9e7b6544c16d970c6538d2223075af71df466a2
Kyaw Zay Yar Soe <kyawzayarsoe@gmail.com>
2025-06-08 16:28:46 +0630

    updated

--- Custom-RTP-Server/websocket_server.c
+++ Custom-RTP-Server/websocket_server.c
@@ -2,13 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libwebsockets.h>
-#include "rtp_server.h" // Added for running variable
+#include "rtp_server.h"
 #include "db.h"
 
 static struct lws_context *context;
 static struct lws *clients[100];
 static int client_count = 0;
 static pthread_mutex_t ws_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Mount configuration for serving static files
+static const struct lws_http_mount mount_output = {
+    .mount_next = NULL,
+    .mountpoint = "/output",
+    .origin = "/app/output",
+    .def = NULL,
+    .protocol = NULL,
+    .cgienv = NULL,
+    .extra_mimetypes = NULL,
+    .interpret = NULL,
+    .cgi_timeout = 0,
+    .cache_max_age = 0,
+    .auth_mask = 0,
+    .cache_reusable = 0,
+    .cache_revalidate = 0,
+    .cache_intermediaries = 0,
+    .origin_protocol = LWSMPRO_FILE,
+    .mountpoint_len = 7,
+};
+
+static const struct lws_http_mount mount_index = {
+    .mount_next = &mount_output,
+    .mountpoint = "/",
+    .origin = "/app/frontend/index.html",
+    .def = "index.html",
+    .protocol = NULL,
+    .cgienv = NULL,
+    .extra_mimetypes = NULL,
+    .interpret = NULL,
+    .cgi_timeout = 0,
+    .cache_max_age = 0,
+    .auth_mask = 0,
+    .cache_reusable = 0,
+    .cache_revalidate = 0,
+    .cache_intermediaries = 0,
+    .origin_protocol = LWSMPRO_FILE,
+    .mountpoint_len = 1,
+};
 
 static void send_json(const char* ip, int port, void* user_data) {
     char json[256];
@@ -58,6 +97,12 @@ static struct lws_protocols protocols[] = {
         0,
         0
     },
+    {
+        "http",
+        lws_callback_http_dummy,
+        0,
+        0
+    },
     { NULL, NULL, 0, 0 }
 };
 
@@ -65,18 +110,12 @@ void start_websocket_server() {
     struct lws_context_creation_info info = {0};
     info.port = atoi(getenv("WEBSOCKET_PORT") ? getenv("WEBSOCKET_PORT") : "8188");
     info.protocols = protocols;
-    info.options = LWS_SERVER_OPTION_EXPLICIT_VHOSTS;
+    info.mounts = &mount_index;
+    info.options = LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
 
     context = lws_create_context(&info);
     if (!context) {
         fprintf(stderr, "lws_create_context failed\n");
-        return;
-    }
-
-    struct lws_vhost *vhost = lws_create_vhost(context, &info);
-    if (!vhost) {
-        fprintf(stderr, "lws_create_vhost failed\n");
-        lws_context_destroy(context);
         return;
     }
 
